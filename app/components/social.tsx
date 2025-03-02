b45dec09bf6a249676db4a45c256c52853090a73
nardiyansah <nardiyansah.jatmiko@gmail.com>
2025-03-02 08:44:04 +0700

    feat(social media): add more socmed button

--- app/components/social.tsx
+++ app/components/social.tsx
@@ -1,11 +1,16 @@
-import { Box, Heading, Link } from "@chakra-ui/react";
-import { FaGithub } from "react-icons/fa6";
+import { Box, Heading, Link, VStack } from "@chakra-ui/react";
+import { FaGithub, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa6";
 
 export default function Social() {
   return (
     <Box>
-      <Heading>Find Me</Heading>
-      <Link href="https://github.com/nardiyansah"><FaGithub />nardiyansah</Link>
+      <Heading py={"4"}>Find Me</Heading>
+      <VStack float={"left"}>
+        <Link href="https://github.com/nardiyansah"><FaGithub />nardiyansah</Link>
+        <Link href="https://www.linkedin.com/in/nardiyansah/"><FaLinkedin />nardiyansah</Link>
+        <Link href="https://x.com/nardiyansahtj"><FaTwitter />nardiyansah</Link>
+        <Link href="https://instagram.com/nardiyansahtj"><FaInstagram />nardiyansah</Link>
+      </VStack>
     </Box>
   )
 }

0aade40d365da3aa3b33d062d5971ed55b929f23
nardiyansah <nardiyansah.jatmiko@gmail.com>
2025-03-05 10:00:04 +0700

    feat(bio): add bio timeline

---
+++ app/components/bio.tsx
@@ -0,0 +1,87 @@
+import { Box, Heading, Span, TimelineConnector, TimelineContent, TimelineItem, TimelineRoot, TimelineTitle } from "@chakra-ui/react";
+
+export default function Bio() {
+  return (
+    <Box py={"3"}>
+      <Heading>Bio</Heading>
+      <TimelineRoot>
+        <TimelineItem>
+          <TimelineContent width={"auto"}>
+            <TimelineTitle whiteSpace={"nowrap"}>Jun 2022</TimelineTitle>
+          </TimelineContent>
+          <TimelineConnector />
+          <TimelineContent>
+            <TimelineTitle>
+              <Span>Software Engineer</Span>
+              <Span color={"fg.muted"}>at</Span>
+              <Span>Tokopedia</Span>
+            </TimelineTitle>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineContent width={"auto"}>
+            <TimelineTitle whiteSpace={"nowrap"}>Feb 2022</TimelineTitle>
+          </TimelineContent>
+          <TimelineConnector />
+          <TimelineContent>
+            <TimelineTitle>
+              <Span>Backend Engineer (Internship)</Span>
+              <Span color={"fg.muted"}>at</Span>
+              <Span>Pegipegi</Span>
+            </TimelineTitle>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineContent width={"auto"}>
+            <TimelineTitle whiteSpace={"nowrap"}>Oct 2021</TimelineTitle>
+          </TimelineContent>
+          <TimelineConnector />
+          <TimelineContent>
+            <TimelineTitle>
+              <Span>Backend Developer (Internship)</Span>
+              <Span color={"fg.muted"}>at</Span>
+              <Span>Photon</Span>
+            </TimelineTitle>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineContent width={"auto"}>
+            <TimelineTitle whiteSpace={"nowrap"}>Jun 2021</TimelineTitle>
+          </TimelineContent>
+          <TimelineConnector />
+          <TimelineContent>
+            <TimelineTitle>
+              <Span>Freelance Web Developer</Span>
+            </TimelineTitle>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineContent width={"auto"}>
+            <TimelineTitle whiteSpace={"nowrap"}>May 2021</TimelineTitle>
+          </TimelineContent>
+          <TimelineConnector />
+          <TimelineContent>
+            <TimelineTitle>
+              <Span>Generasi Gigih Participant - Backend Engineer Path</Span>
+              <Span color={"fg.muted"}>at</Span>
+              <Span>Yayasan Anak Bangsa Bisa</Span>
+            </TimelineTitle>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineContent width={"auto"}>
+            <TimelineTitle whiteSpace={"nowrap"}>Dec 2021</TimelineTitle>
+          </TimelineContent>
+          <TimelineConnector />
+          <TimelineContent>
+            <TimelineTitle>
+              <Span>Graduated from Informatic Engineering Bachelor's Degree</Span>
+              <Span color={"fg.muted"}>at</Span>
+              <Span>Institution Technology of Sumatera</Span>
+            </TimelineTitle>
+          </TimelineContent>
+        </TimelineItem>
+      </TimelineRoot>
+    </Box>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import Greeting from "@/app/components/greeting";
 import Social from "@/app/components/social";
+import Bio from "@/app/components/bio"
 
 export default function Home() {
   return (
     <Box>
       <Greeting />
+      <Bio />
       <Social />
     </Box>
   );

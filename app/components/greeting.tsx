b1899409c6915d3ebf4bfc85819d914aec789c67
nardiyansah <nardiyansah.jatmiko@bytedance.com>
2025-03-16 08:44:16 +0700

    feat(greeting): add greeting pharagraph

--- app/components/greeting.tsx
+++ app/components/greeting.tsx
@@ -3,8 +3,12 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 export default function Greeting() {
   return (
     <Box>
-      <Heading>Nardiyansah Tri Jatmiko</Heading>
-      <Text>I'm a software engineer from Indonesia</Text>
+      <Heading>Hello, I'm Nardiyansah Tri Jatmiko</Heading>
+      <Text py={1}>You can call me Nardi for short</Text>
+      <Text py={1}>I grew up in Lampung, Indonesia and graduated from the Institute of Technology of Sumatera with a Bachelor’s degree in Computer Science. Currently, I’m working as a Software Engineer at Tokopedia, where I get to build and maintain functionalities of shop. Before Tokopedia, I had the chance to work as a Backend Engineer Intern at Pegipegi and gain experience at other awesome places like Photon and Banopolis Inovasi Kendara.</Text>
+      <Text py={1}>I’m passionate about exploring everything tech-related, and I’ve been fortunate to work on some exciting projects along the way. From freelancing as a Web Developer to being a Lab Assistant for the Introduction to Computer Software course at my alma mater, I’ve always enjoyed learning and sharing knowledge.</Text>
+      <Text py={1}>One of my proudest moments was being selected as 1 of 60 candidates out of 1,000 for an internship at Yayasan Anak Bangsa Bisa—a truly rewarding experience that helped me grow both personally and professionally.</Text>
+      <Text py={1}>When I’m not coding, you can find me exploring new techologies and reading books. I’m always excited to connect, collaborate, or just chat about tech, life, or anything in between.</Text>
     </Box>
   )
 }
